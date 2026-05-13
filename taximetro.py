b5b9a5e7f07e2968a43d868a2e60e138ed44e89f
Johans Salas <johsx01@gmail.com>
2026-05-13 15:24:36 +0200

    Se corrige lógica def_iniciar nuevamente

--- taximetro.py
+++ taximetro.py
@@ -709,10 +709,7 @@ class AppTaximetro(tk.Tk):
         self.btn_iniciar.config(state="disabled")
         self.btn_toggle.config(state="normal")
         self.btn_fin.config(state="normal")
-        self._actualizar_estado()
-        
-        # Bloquear cambio de servicio durante el trayecto
-        for btn in self._btns_servicio.values():
+        for btn in self._btns_servicio.values(): # Bloquear cambio de servicio durante el trayecto
             btn.config(state="disabled")
         self._actualizar_estado()
 
